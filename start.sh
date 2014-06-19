45d56ba217eeb55074e88fd09c89929dee3d3763
sahardge <sahardge1@catamount.wcu.edu>
2014-06-19 15:30:47 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-IP=`/usr/bin/etcdctl get /rexsterIP`
+IP=`/usr/bin/etcdctl --peers 10.1.42.1:4001 get /rexsterIP`
+
 
 mv config/rexster.xml config/rexster.xml.orig
 cat config/rexster.xml.orig | sed -e "/<base-uri>/s/localhost/""$IP""/" > config/rexster.xml
