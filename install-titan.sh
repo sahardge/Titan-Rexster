9cbac2c60128af6b7ec15d456f9004740be3c2fb
sahardge <sahardge1@catamount.wcu.edu>
2014-06-19 11:29:07 -0400

    Update install-titan.sh

--- install-titan.sh
+++ install-titan.sh
@@ -1,9 +1,10 @@
 # Argument is the version to install, or default value
 VERSION=${1:-'0.4.4'}
-IP=${2:-'10.10.10.100'}
+IP=`hostname --ip-address`
 BACKEND=cassandra #"cassandra" includes rextser, cassandra, and all other backend/indexing support
 TITAN=titan-${BACKEND}-${VERSION}
 REXSTERHOME=/rexster
+
 
 echo "Installing Rexster..."
 wget -q http://tinkerpop.com/downloads/rexster/rexster-server-2.5.0.zip && unzip rexster-server-2.5.0.zip -d /tmp/rexster
@@ -12,7 +13,7 @@ cd /rexster
 mkdir -p ${REXSTERHOME}/ext/titan
 cd ${REXSTERHOME}
 mv config/rexster.xml config/rexster.xml.orig
-cat config/rexster.xml.orig | sed -e "/<base-uri>/s/localhost/172.17.0.2/" > config/rexster.xml
+cat config/rexster.xml.orig | sed -e "/<base-uri>/s/localhost/""$IP""/" > config/rexster.xml
 #bump rexster heap size
 cp bin/rexster.sh bin/rexster.sh.orig
 sudo bash -c 'cat bin/rexster.sh.orig | sed -e "/-server/s/-Xms128m -Xmx512m/-Xms128m -Xmx2048m -XX:MaxPermSize=256m/" > bin/rexster.sh'
