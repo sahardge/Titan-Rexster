e4ed2dc6a7b11d7650de02656688d0ff9b79e2ca
sahardge <sahardge1@catamount.wcu.edu>
2014-07-02 10:24:24 -0400

    Update install-titan.sh

--- install-titan.sh
+++ install-titan.sh
@@ -7,8 +7,8 @@ REXSTERHOME=/rexster
 
 
 echo "Installing Rexster..."
-wget -q http://tinkerpop.com/downloads/rexster/rexster-server-2.5.0.zip && unzip rexster-server-2.5.0.zip -d /tmp/rexster
-mv /tmp/rexster/rexster-server-2.5.0/ /rexster
+wget -q http://tinkerpop.com/downloads/rexster/rexster-server-2.4.0.zip && unzip rexster-server-2.4.0.zip -d /tmp/rexster
+mv /tmp/rexster/rexster-server-2.4.0/ /rexster
 cd /rexster
 mkdir -p ${REXSTERHOME}/ext/titan
 cd ${REXSTERHOME}
